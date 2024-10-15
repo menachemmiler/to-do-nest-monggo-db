ca8e75b2bb3c8d31e3bda2a8b39be84b7d7df3cf
menachem <AM0556727820@GMAIL.COM>
2024-10-15 14:54:55 +0300

    with global pipe validation

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('api notes')
     .setDescription('the notes API')
--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,1 +1,12 @@
-export class CreateNoteDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+import { IsString, MinLength } from 'class-validator'; //מחלקה שאחראית על ולידציה לסוגי המידע(שהסוג שהתקבל תואם לסוג שמצופה)
+export class CreateNoteDto {
+  @ApiProperty()
+  @IsString({ message: 'the title is invalid' })
+  title: string;
+
+  @ApiProperty()
+  @MinLength(6, { message: 'the content must to be at last 6 chars' })
+  content: string;
+}
